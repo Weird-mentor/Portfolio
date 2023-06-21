e8510875a10e6c87c41723462a98dca1c4b8ed18
Weird-mentor <104635922+Weird-mentor@users.noreply.github.com>
2023-06-21 20:08:23 +0530

    Add files via upload

--- portfolio website/portfolio website/script.js
+++ portfolio website/portfolio website/script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function(){
 
   $('#menu').click(function(){
@@ -35,39 +37,18 @@ $(document).ready(function(){
 
   });
 
+
 });
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'uil-sun'
 
-// Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
 
-// We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
-
-// We validate if the user previously chose a topic
-if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
-}
-themeButton.addEventListener('click', () => {
-  // Add or remove the dark / icon theme
-  document.body.classList.toggle(darkTheme)
-  themeButton.classList.toggle(iconTheme)
-  // We save the theme and the current icon that the user chose
-  localStorage.setItem('selected-theme', getCurrentTheme())
-  localStorage.setItem('selected-icon', getCurrentIcon())
-})
 document.addEventListener("click" ,(e)=> {
   if(e.target.classList.contains("work_btn")){
     togglePortPopup();
     portItemDetails(e.target.parentElement);
   }
 })
+
+
 function togglePortPopup(){
   document.querySelector(".port_popup").classList.toggle("open");
 }
@@ -78,3 +59,4 @@ function portItemDetails(portfolioItem)
   document.querySelector(".port_popup-subtitle span").innerHTML = portfolioItem.querySelector(".work_title").innerHTML;
   document.querySelector(".port_popup-body").innerHTML=portfolioItem.querySelector(".port").innerHTML;
 }
+
